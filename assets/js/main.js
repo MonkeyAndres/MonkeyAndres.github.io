481542aa8bd55962d89a21630f7ee0598fc2429d
Andres Martin Angulo <monkeyandres@protonmail.com>
2024-06-15 17:13:34 +0200

    feat: auto-dark-light mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,35 @@
   const body = document.body;
   const lamp = document.getElementById("mode");
 
+  const setTheme = (state) => {
+    localStorage.setItem("theme", state);
+    document.body.setAttribute("data-theme", state);
+  }
+
   const toggleTheme = (state) => {
-    if (state === "dark") {
-      localStorage.setItem("theme", "light");
-      body.removeAttribute("data-theme");
-    } else if (state === "light") {
-      localStorage.setItem("theme", "dark");
-      body.setAttribute("data-theme", "dark");
-    } else {
-      initTheme(state);
-    }
+    state === "dark" ? setTheme("light") : setTheme("dark");
   };
 
   lamp.addEventListener("click", () =>
     toggleTheme(localStorage.getItem("theme"))
   );
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const userThemePreference = localStorage.getItem("theme");
+
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (!!userThemePreference && userThemePreference.length > 0) {
+      setTheme(userThemePreference);
+    } else {
+      console.log('No theme preference found in local storage. Setting theme based on user preference.')
+      setTheme(prefersDarkScheme.matches ? 'dark' : 'light');
+    }
+
+    prefersDarkScheme.addEventListener('change', (mediaQuery) => {
+      setTheme(mediaQuery.matches ? 'dark' : 'light');
+    });
+  })
 
   // Blur the content when the menu is open
   const cbox = document.getElementById("menu-trigger");
